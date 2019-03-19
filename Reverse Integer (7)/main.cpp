ea9d192eb13bb92cb2180a0f1227f94422ebdf02
Runxuan Jiang <runxuanj@umich.edu>
2019-03-19 16:35:52 -0400

    added template and updated files for reverse integer

---
+++ Reverse Integer (7)/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <map>
+#include <utility>
+
+using namespace std;
+
+
+class Solution {
+public:
+	int reverse(int x) {
+		int ans;
+		bool positive;
+		if (abs(x) / 10 == 0) return x;
+		positive = (x < 0) ? false : true;
+		for (int i = 0; i < )
+	}
+
+
+	int num_digits(int x) {
+		int ans = 0;
+		while (x != 0)
+	}
+};
+
+int main() {
+
+}
---
+++ leetcode_template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <utility>
+
+using namespace std;
+
+//Insert code stub here
+
+int main() {
+
+}
