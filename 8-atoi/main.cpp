231732ad93bf68ea2806a54c437cf8029bff762e
Runxuan Jiang <runxuan.jiang@gmail.com>
2019-03-19 18:25:16 -0400

    started atoi

---
+++ 8-atoi/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <utility>
+
+using namespace std;
+
+class Solution {
+public:
+	int myAtoi(string str) {
+
+	}
+};
+
+int main() {
+
+}
