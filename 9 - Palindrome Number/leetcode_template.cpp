1c99eeed2fbe75f40812a5562b3e2bc06a78ee7b
Runxuan Jiang <runxuan.jiang@gmail.com>
2019-03-20 22:45:02 -0400

    started palindrome integer

---
+++ 9 - Palindrome Number/leetcode_template.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <utility>
+
+using namespace std;
+
+//Insert code stub here
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		bool ans;
+		if (x < 0) return false;
+		deque<int> digits;
+		while (x > 0) {
+			for ()
+		}
+	}
+};
+
+int main() {
+
+}
