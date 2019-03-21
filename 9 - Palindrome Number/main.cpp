10ef817c74d4b58239999ec7180301dc2bedd9ae
Runxuan Jiang <runxuan.jiang@gmail.com>
2019-03-21 13:17:32 -0400

    finished palindrome number

--- 9 - Palindrome Number/main.cpp
+++ 9 - Palindrome Number/main.cpp
@@ -14,15 +14,15 @@ public:
 	bool isPalindrome(int x) {
 		bool ans;
 		if (x < 0) return false;
-		deque<int> digits;
+		vector<int> digits;
 		while (x > 0) {
-			digits.push_front(x % 10);
+			digits.push_back(x % 10);
 			x /= 10;
 		}
-		auto i = digits.begin();
-		auto j = digits.begin() + (digits.size() - 1);
-		while (i != j) {
-			if (*i != *j) return false;
+		int i = 0;
+		int j = digits.size() - 1;
+		while (i < j) {
+			if (digits[i] != digits[j]) return false;
 			++i;
 			--j;
 		}
