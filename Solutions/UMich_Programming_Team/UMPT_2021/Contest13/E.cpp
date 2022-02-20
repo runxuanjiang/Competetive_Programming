8b426fcb4f5e09552b19e211927830e5e4bb7789
Runxuan Jiang <runxuan.jiang@gmail.com>
2022-02-20 00:05:27 -0500

    solved expecting rain

--- Solutions/UMich_Programming_Team/UMPT_2021/Contest13/E.cpp
+++ Solutions/UMich_Programming_Team/UMPT_2021/Contest13/E.cpp
@@ -8,11 +8,15 @@ using namespace std;
 
 using ll = long long;
 using ull = unsigned long long;
-using ld = long double;
+using ld = double;
 const int MOD = 1000000007;
 
 #define fast ios::sync_with_stdio(false); cin.tie(nullptr)
 #define tests int testCount; cin >> testCount; while(testCount--)
+
+ld dp[1001][10001];
+bool cover[1001];
+bool coverseg[1001];
 
 struct Cloud {
     ll start;
@@ -25,46 +29,9 @@ struct Cloud {
 
 bool comp(const pair<ll, pair<ll, ll>> &l, const pair<ll, pair<ll, ll>> &r) {
     if (l.second.first == r.second.first) {
-        return l.first < r.first;
+        return l.first > r.first;
     }
     return l.second.first < r.second.first;
-}
-
-ld recurse(int dist, int time, const vector<ld> &erain, const vector<bool> &cover, vector<vector<ld>> &dp) {
-    if (time >= erain.size() - 1) {
-        if (dist >= cover.size() - 1) {
-            dp[dist][time] = (1-cover.back()) * erain.back();
-            return (1-cover.back()) * erain.back();
-        }
-        else {
-            dp[dist][time] = 1000000000000;
-            return 1000000000000;
-        }
-    }
-    if (dist >= cover.size() - 1) {
-        if (cover.back()) {
-            dp[dist][time] = 0;
-            return 0;
-        }
-        else {
-            dp[dist][time] = erain[time] + recurse(dist, time + 1, erain, cover, dp);
-            return erain[time] + recurse(dist, time + 1, erain, cover, dp);
-        }
-    }
-
-
-    if (dp[dist][time] != -1) {
-        return dp[dist][time];
-    }
-
-    if (cover[dist]) {
-        dp[dist][time] = min(recurse(dist, time+1, erain, cover, dp), recurse(dist+1, time+1, erain, cover, dp));
-    }
-    else {
-        dp[dist][time] = erain[time] + min(recurse(dist, time+1, erain, cover, dp), recurse(dist+1, time+1, erain, cover, dp));
-    }
-
-    return dp[dist][time];
 }
 
 int main() {
@@ -96,40 +63,45 @@ int main() {
 
     vector<ld> erain(t+1, 0);
     ld currsum = 0;
-    ld currnum = 0;
     int idx = -1;
     for (int i = 0; i <= t; ++i) {
-        while (idx + 1 < sweep.size() && (sweep[idx + 1].second.first < t || (sweep[idx+1].second.first == t && !sweep[idx+1].first))) {
+        while (idx + 1 < sweep.size() && (sweep[idx + 1].second.first <= i)) {
             ++idx;
             Cloud &cloud = clouds[sweep[idx].second.second];
-            if (sweep[idx+1].first) {
-                --currnum;
+            if (sweep[idx].first) {
                 currsum -= cloud.amount * cloud.ev;
             }
             else {
-                ++currnum;
                 currsum += cloud.amount * cloud.ev;
             }
         }
-        erain[i] = currnum ? currsum / currnum : 0;
+        erain[i] = currsum;
     }
 
-    vector<bool> cover(d+1, 0);
     cover[0] = true;
     for (auto roof : roofs) {
         for (int j = roof.first; j <= roof.second; ++j) {
             cover[j] = true;
+            if (j > roof.first) {
+                coverseg[j-1] = true;
+            }
         }
     }
 
-    vector<vector<ld>> dp(d+1, vector<ld>(t+1, -1));
-    cout << setprecision(10);
-    ld res = recurse(0, 0, erain, cover, dp);
-    for (auto i : dp) {
-        for (auto j : i) {
-            cout << j << " ";
-        }
-        cout << endl;
+    // vector<vector<ld>> dp(d+1, vector<ld>(t+1, 0));
+    ld curr = 0;
+    for (int j = t; j >= 0; --j) {
+        curr += (1 - (int) cover[d]) * erain[j];
+        dp[d][j] = curr;
     }
-    cout << res << endl;
+    for (int i = d-1; i >= 0; --i) {
+        int diff = d - i;
+        dp[i][t-diff + 1] = 1000000000000;
+        for (int j = t-diff; j >= i; --j) {
+            dp[i][j] = min((1 - (int) cover[i]) * erain[j] + dp[i][j+1], (1 - (int) coverseg[i]) * erain[j] + dp[i+1][j+1]);
+        }
+    }
+
+    cout << setprecision(10);
+    cout << dp[0][0] << endl;
 }
