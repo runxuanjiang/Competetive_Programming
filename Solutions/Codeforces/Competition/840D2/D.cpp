e83800faea9dfd854d9b6c9532f1aaad64476c2f
runxuanjiang <runxuan.jiang@gmail.com>
2023-01-03 20:30:08 -0500

    finished d and e for 840 d2

--- Solutions/Codeforces/Competition/840D2/D.cpp
+++ Solutions/Codeforces/Competition/840D2/D.cpp
@@ -14,6 +14,77 @@ const int MOD = 1000000007;
 #define fast ios::sync_with_stdio(false); cin.tie(nullptr)
 #define tests int testCount; cin >> testCount; while(testCount--)
 
+vector<vector<ll>> pascal(101, vector<ll>(101, 0));
+
+ll choose(ll n, ll k) {
+    if (k < 0 || k > n) {
+        return 0;
+    }
+
+    return pascal[n][k];
+
+}
+
 int main() {
     fast;
+
+    
+    for (int i = 0; i <= 100; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            if (j == 0 || j == i) {
+                pascal[i][j] = 1;
+            }
+            else {
+                pascal[i][j] = (pascal[i-1][j-1] + pascal[i-1][j]) % MOD;
+            }
+        }
+    }
+    tests {
+        ll n;
+        cin >> n;
+        ll i, j;
+        cin >> i >> j;
+        ll x, y;
+        cin >> x >> y;
+
+        if (x > y) {
+            swap(x, y);
+            ll oi = i;
+            i = n - j + 1;
+            j = n - oi + 1;
+        }
+
+        if (y == n) {
+            if (j == n) {
+                cout << 0 << endl;
+            }
+            else {
+                ll res = 1;
+                res = (res*choose(y-x-1, j-i-1)) % MOD;
+                res = (res*choose(x-1, i-1)) % MOD;
+                cout << res << endl;
+            }
+        }
+        else {
+            ll res = 0;
+            for (int k = i+1; k < n; ++k) {
+                if (k > j) {
+                    ll tempres = 1;
+                    tempres = (tempres * choose(n-y-1, k-j-1)) % MOD;
+                    tempres = (tempres * choose(y-x-1, j-i-1)) % MOD;
+                    tempres = (tempres * choose(x-1, i-1)) % MOD;
+                    res = (res + tempres) % MOD;
+                }
+                else if (k < j) {
+                    ll tempres = 1;
+                    tempres = (tempres * choose(n-y-1, j-k-1)) % MOD;
+                    tempres = (tempres * choose(x-1, i-1)) % MOD;
+                    tempres = (tempres * choose(y-x-1, n-j-x+i)) % MOD;
+                    res = (res + tempres) % MOD;
+                }
+            }
+
+            cout << res << endl;
+        }
+    }
 }
--- Solutions/Codeforces/Competition/840D2/E.cpp
+++ Solutions/Codeforces/Competition/840D2/E.cpp
@@ -16,4 +16,62 @@ const int MOD = 1000000007;
 
 int main() {
     fast;
+    ll n;
+    cin >> n;
+
+    vector<ll> nodesets;
+    vector<ll> nodesetsize;
+    for (ll i = 2; i * (i-1)/2 <= n; ++i) {
+        nodesets.push_back(i*(i-1)/2);
+        nodesetsize.push_back(i);
+    }
+
+    vector<ll> dp(n+1);
+    for (int i = 0; i <= n; ++i) {
+        dp[i] = 2*i;
+    }
+    for (int i = 0; i < nodesets.size(); ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (j + nodesets[i] > n) {
+                break;
+            }
+
+            dp[j + nodesets[i]] = min(dp[j+nodesets[i]], dp[j] + nodesetsize[i]);
+        }
+    }
+
+    vector<ll> nodes;
+    ll curr = n;
+    ll total = 0;
+    while (curr) {
+        ll currmin = 2 + dp[curr-1];
+        ll currminidx = 0;
+        for (int i = 0; i < nodesets.size(); ++i) {
+            if (nodesets[i] > curr) {
+                break;
+            }
+            if (nodesetsize[i] + dp[curr-nodesets[i]] <= currmin) {
+                currmin = nodesetsize[i] + dp[curr-nodesets[i]];
+                currminidx = i;
+            }
+            
+        }
+        nodes.push_back(nodesetsize[currminidx]);
+        total += nodesetsize[currminidx];
+        curr -= nodesets[currminidx];
+    }
+
+    sort(nodes.begin(), nodes.end());
+
+    ll res1 = total;
+    ll res2 = 0;
+    for (auto i : nodes) {
+        total -= i;
+        res2 += i * total;
+    }
+
+    cout << res1 << " " << res2 << endl;
+
+
+
 }
