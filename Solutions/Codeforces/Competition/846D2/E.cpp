587df4577a86ffd60b16a7e1774c39d8ab26f875
runxuanjiang <runxuan.jiang@gmail.com>
2025-01-31 23:02:11 -0500

    finished 846D2

--- Solutions/Codeforces/Competition/846D2/E.cpp
+++ Solutions/Codeforces/Competition/846D2/E.cpp
@@ -14,14 +14,39 @@ const int MOD = 1000000007;
 #define fast ios::sync_with_stdio(false); cin.tie(nullptr)
 #define tests int testCount; cin >> testCount; while(testCount--)
 
+
 int main() {
     fast;
     tests {
+        // cerr << "tests remaining: " << testCount << endl;
         ll l, r;
         cin >> l >> r;
 
-        
+        ll res = 0;
+        ll ub2 = r/2;
+        if (ub2 >= l) {
+            res += (ub2-l+1);
+        }
 
-        
+        ll lb = 1, ub = 1;
+        while (lb < l) {
+            ll proj = (l % lb == 0) ? l/lb : (l/lb) + 1;
+            ll nproj = proj+1;
+            ll lub = r/nproj;
+            if (lub >= lb) {
+                res += (min(lub, ub) - lb + 1);
+            }
+
+            // cerr << "(lb, ub, proj, nproj, res): " << lb << " " << ub << " " << proj << " " << nproj << " " << res << endl;
+
+            lb = ub+1;
+            if (lb < l) {
+                ll cproj = (l % lb == 0) ? l/lb : (l/lb) + 1;
+                ll cnproj = cproj - 1;
+                ub = (l % cnproj == 0) ? (l/cnproj)-1 : l/cnproj;
+            }
+        }
+
+        cout << res << endl;
     }
 }
--- Solutions/Codeforces/Competition/846D2/F.cpp
+++ Solutions/Codeforces/Competition/846D2/F.cpp
@@ -14,6 +14,105 @@ const int MOD = 1000000007;
 #define fast ios::sync_with_stdio(false); cin.tie(nullptr)
 #define tests int testCount; cin >> testCount; while(testCount--)
 
+void dfs(ll &curr, ll &num, ll idx, const vector<ll> &primes, vector<pair<ll, ll>> &count, ll &c, ll &s, const ll index) {
+    curr *= primes[idx];
+    ++num;
+
+    if (num % 2) {
+        c -= count[curr].first;
+        s -= count[curr].second;
+    }
+    else {
+        c += count[curr].first;
+        s += count[curr].second;
+    }
+
+
+    count[curr].first++;
+    count[curr].second += index;
+
+
+    for (int j = idx+1; j < primes.size(); ++j) {
+        dfs(curr, num, j, primes, count, c, s, index);
+    }
+
+    curr /= primes[idx];
+    --num;
+}
+
+vector<ll> getPrimes(ll num, const vector<ll> &primes, const vector<bool> &sieve) {
+    vector<ll> cprimes;
+    for (auto p : primes) {
+        if (num == 1) {
+            break;
+        }
+        if (sieve[num]) {
+            cprimes.push_back(num);
+            break;
+        }
+        if (num % p == 0) {
+            cprimes.push_back(p);
+            while (num % p == 0) {
+                num /= p;
+            }
+        }
+    }
+    return cprimes;
+}
+
 int main() {
     fast;
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+
+    vector<pair<ll, ll>> count(300001, {0, 0});
+
+    vector<bool> sieve(300001, 1);
+    sieve[1] = 0;
+    vector<ll> primes;
+    for (ll i = 2; i <= 300000; ++i) {
+        if (sieve[i]) {
+            primes.push_back(i);
+            for (ll j = 2; j*i <= 300000; ++j) {
+                sieve[i*j] = 0;
+            }
+        }
+    }
+
+    ll res = 0;
+
+    for (int i = 0; i < n; ++i) {
+        vector<ll> cprimes = getPrimes(a[i], primes, sieve);
+
+
+        // cerr << "currently on " << a[i] << endl;
+        // for (auto p : cprimes) {
+        //     cerr << p << " ";
+        // }
+        // cerr << endl;
+
+        ll c = count[1].first;
+        ll s = count[1].second;
+        count[1].first++;
+        count[1].second += i;
+        ll curr = 1;
+        ll num = 0;
+        for (ll j = 0; j < cprimes.size(); ++j) {
+            dfs(curr, num, j, cprimes, count, c, s, i);
+        }
+
+
+        res += (c * i - s - c);
+    }
+
+    cout << res << endl;
+
+
 }
