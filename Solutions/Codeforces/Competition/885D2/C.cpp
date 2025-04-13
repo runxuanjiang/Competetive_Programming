2e69acd32e0401d22b2484b7b58da3e4a5e9de31
Runxuan Jiang <runxuan.jiang@gmail.com>
2025-04-13 16:51:52 -0400

    finished C and D of 885 D2

--- Solutions/Codeforces/Competition/885D2/C.cpp
+++ Solutions/Codeforces/Competition/885D2/C.cpp
@@ -14,6 +14,64 @@ const int MOD = 1000000007;
 #define fast ios::sync_with_stdio(false); cin.tie(nullptr)
 #define tests int testCount; cin >> testCount; while(testCount--)
 
+ll calculate_zero_frequency(long long a, long long b) {
+    if (a == 0 && b == 0) {
+        return -1;
+    }
+
+    if (a == 0) {
+        return 0;
+    }
+    if (b == 0) {
+        return 1;
+    }
+    if (a == b) {
+        return 2;
+    }
+
+    if (a < b) {
+        return (1 + calculate_zero_frequency(b, b-a)) % 3;
+    }
+
+    ll iterations = (a/b);
+    if (iterations % 2 == 0) {
+        return (1 + calculate_zero_frequency(b, b - (a%b))) % 3;
+    }
+    else {
+        return (1 + calculate_zero_frequency(b, a - (iterations) * b)) % 3;
+    }
+
+
+}
+
 int main() {
     fast;
+    tests {
+        ll n;
+        cin >> n;
+        vector<ll> a(n), b(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> b[i];
+        }
+
+        vector<bool> zero_frequencies(3, 0);
+        for (int i = 0; i < n; ++i) {
+            ll zero_frequency = calculate_zero_frequency(a[i], b[i]);
+            cerr << zero_frequency << " ";
+            if (zero_frequency >= 0) {
+                zero_frequencies[zero_frequency] = true;
+            }
+        }
+        cerr << endl;
+
+
+        if (count(zero_frequencies.begin(), zero_frequencies.end(), true) > 1) {
+            cout << "NO\n";
+        } else {
+            cout << "YES\n";
+        }
+    }
 }
--- Solutions/Codeforces/Competition/885D2/D.cpp
+++ Solutions/Codeforces/Competition/885D2/D.cpp
@@ -14,6 +14,62 @@ const int MOD = 1000000007;
 #define fast ios::sync_with_stdio(false); cin.tie(nullptr)
 #define tests int testCount; cin >> testCount; while(testCount--)
 
+const unordered_set<ll> cycle_elements = {2, 4, 6, 8};
+
+ll search_maximum(ll s, ll k) {
+    ll res = s*k;
+    ll l = 0, r = k/4;
+    while (l <= r) {
+        ll m = l + (r-l)/2;
+
+        ll m_discount = (s + 20*m) * (k - 4*m);
+        ll next_m_discount = (s + 20 * (m+1)) * (k - 4*(m+1));
+        res = max(res, m_discount);
+        if (m*4 >= k || m_discount > next_m_discount) {
+            r = m-1;
+        }
+        else {
+            l = m+1;
+        }
+    }
+    
+    return res;
+}
+
 int main() {
     fast;
+    tests {
+        ll s, k;
+        cin >> s >> k;
+
+        if (s % 10 == 0) {
+            cout << k * s << endl;
+        }
+        else if (s % 10 == 5) {
+            cout << max(k*s, (k-1)*(s+5)) << endl;
+        }
+        else {
+            ll res = k*s;
+            ll cycle_count = 0;
+            for (ll i = 1; i <= k; ++i) {
+                s = s + (s % 10);
+
+                if (cycle_elements.find(s % 10) != cycle_elements.end()) {
+                    cycle_count++;
+                    res = max(res, search_maximum(s, k-i));
+                }
+                else {
+                    res = max(res, s * (k-i));
+                }
+
+                if (cycle_count == 4) {
+                    break;
+                }
+
+            }
+            cout << res << endl;
+        }
+
+
+    }
 }
