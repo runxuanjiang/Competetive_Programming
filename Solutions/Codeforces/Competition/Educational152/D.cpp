45c0b3ec4ec8d80bc2f742ae963ece11c49ae98b
Runxuan Jiang <runxuan.jiang@gmail.com>
2025-05-10 13:24:06 -0400

    finished educational152

--- Solutions/Codeforces/Competition/Educational152/D.cpp
+++ Solutions/Codeforces/Competition/Educational152/D.cpp
@@ -16,4 +16,44 @@ const int MOD = 1000000007;
 
 int main() {
     fast;
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    ll leading_zero = 0;
+    ll res = 0;
+    for (int i = 0; i < n; ++i) {
+        if (a[i] == 0) {
+            ++leading_zero;
+        }
+        else {
+            ++res;
+            int j = i;
+            ll max_num = a[i];
+            while (j+1 < n && a[j+1] > 0) {
+                ++j;
+                max_num = max(max_num, a[j]);
+            }
+
+            if (leading_zero > 0) {
+                res += leading_zero - 1;
+            }
+            if (max_num == 2 || leading_zero == 0) {
+                leading_zero = -1;
+            }
+            else {
+                leading_zero = 0;
+            }
+            i = j;
+        }
+    }
+
+    if (leading_zero > 0) {
+        res += leading_zero;
+    }
+    cout << res << endl;
+
 }
