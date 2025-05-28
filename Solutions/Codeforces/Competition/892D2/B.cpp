827f11295cceed16f80a7bb80adf9ef2fa3f1a0d
Runxuan Jiang <runxuan.jiang@gmail.com>
2025-05-27 21:38:48 -0400

    started 892d2

--- Solutions/Codeforces/Competition/892D2/B.cpp
+++ Solutions/Codeforces/Competition/892D2/B.cpp
@@ -16,4 +16,45 @@ const int MOD = 1000000007;
 
 int main() {
     fast;
+    tests {
+        ll n;
+        cin >> n;
+        vector<vector<ll>> arrays;
+        for (int i = 0; i < n; ++i) {
+            ll m;
+            cin >> m;
+            vector<ll> a(m);
+            for (int j = 0; j < m; ++j) {
+                cin >> a[j];
+            }
+
+            sort(a.begin(), a.end());
+            arrays.push_back(move(a));
+        }
+
+        vector<ll> second_smallest;
+        ll smallest = arrays[0][0];
+
+        for (int i = 0; i < n; ++i) {
+            second_smallest.push_back(arrays[i][1]);
+            smallest = min(smallest, arrays[i][0]);
+        }
+
+        sort(second_smallest.begin(), second_smallest.end());
+
+        ll res = smallest;
+        for (int i = 1; i < n; ++i) {
+            res += second_smallest[i];
+        }
+
+        cout << res << endl;
+
+
+
+
+
+
+
+
+    }
 }
--- Solutions/Codeforces/Competition/892D2/C.cpp
+++ Solutions/Codeforces/Competition/892D2/C.cpp
@@ -16,4 +16,33 @@ const int MOD = 1000000007;
 
 int main() {
     fast;
+
+    tests {
+        ll n;
+        cin >> n;
+
+        ll res = 0;
+
+        for (int i = 0; i <= n; ++i) {
+            vector<ll> products;
+            for (int j = 1; j <= i; ++j) {
+                products.push_back(j*j);
+            }
+            ll curr = n;
+            for (int j = i+1; j <= n; ++j) {
+                products.push_back(j*curr);
+                --curr;
+            }
+
+            sort(products.begin(), products.end());
+            ll curres = 0;
+            for (int j = 0; j+1 < products.size(); ++j) {
+                curres += products[j];
+            }
+
+            res = max(res, curres);
+        }
+
+        cout << res << endl;
+    }
 }
