d552c432dbc3456f134c00d7003e4629ae0f0b65
runxuanjiang <runxuan.jiang@gmail.com>
2025-11-11 19:01:55 -0500

    codeton6 C

--- Solutions/Codeforces/Competition/CodeTON6/C.cpp
+++ Solutions/Codeforces/Competition/CodeTON6/C.cpp
@@ -16,4 +16,44 @@ const int MOD = 1000000007;
 
 int main() {
     fast;
+    tests {
+        ll n, k; 
+        cin >> n >> k;
+
+        vector<ll> a(n);
+        for (ll &i : a) cin >> i;
+
+        vector<bool> used(n, false);
+        ll lb = 0, ub = n-1;
+
+        vector<ll> indices(n, 0);
+        iota(indices.begin(), indices.end(), 0);
+        sort(indices.begin(), indices.end(), [&a](const ll &l, const ll &r) {
+            return a[l] < a[r];
+        });
+
+        vector<ll> res(k+1, 0);
+
+        for (auto i : indices) {
+            while (used[lb]) {
+                ++lb;
+            }
+            while (used[ub]) {
+                --ub;
+            }
+
+            if (res[a[i]] == 0) {
+                res[a[i]] = (ub-lb+1)*2;
+            }
+            used[i] = true;
+            
+        }
+        for (int i = 1; i <=k; ++i) {
+            cout << res[i] << " ";
+        }
+        cout << endl;
+
+        
+
+    }
 }
